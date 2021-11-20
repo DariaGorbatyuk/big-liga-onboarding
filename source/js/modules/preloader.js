cb776ac8f9e6ae1c5402c6e7d449c41e797362d4
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-11-20 21:41:34 +0300

    Исправляет lightning на mobile

--- source/js/modules/preloader.js
+++ source/js/modules/preloader.js
@@ -6,12 +6,14 @@ export function initPreloader() {
   const col1 = document.querySelector('.main-page__col-1');
   const lightnings = document.querySelectorAll('.lightning');
   const preloaderLogo = document.querySelector('.preloader__logo');
+  const preloaderText = document.querySelector('.preloader__text');
   const classPreloadHide = 'preloader--hide';
   const classPreloadLogo = 'preloader__logo--animate';
   const classPreloadOpacity = 'preloader--opacity';
+  const classPreloadTextAnimate = 'preloader__text--animate';
   const col1AnimationClass = 'main-page__col-1--animation';
   const col2AnimationClass = 'main-page__col-2--animation';
-  const colors = ['lightning--purple', 'lightning--pink', 'lightning--blue'];
+  const colors = ['lightning--purple', 'lightning--pink', 'lightning--blue', 'lightning--white'];
   const colorChangeTimer = 1500;
   const preloadTimer = 600;
 
@@ -35,11 +37,21 @@ export function initPreloader() {
   }
 
   function animateDino() {
+    let changedColors;
+    if (isDesktopState) {
+      changedColors = colors.filter((color) => {
+        return color !== 'lightning--white';
+      });
+    } else {
+      changedColors = colors.filter((color) => {
+        return color !== 'lightning--blue';
+      });
+    }
     lightnings.forEach((item) => {
-      const random = Math.floor(Math.random() * colors.length);
+      const random = Math.floor(Math.random() * changedColors.length);
       colors.forEach((color) => {
         item.classList.remove(color);
-        item.classList.add(colors[random]);
+        item.classList.add(changedColors[random]);
       });
     });
   }
@@ -47,6 +59,7 @@ export function initPreloader() {
   function animatePreloaderLogo() {
     preloaderLogo.classList.add(classPreloadLogo);
     preloader.classList.add(classPreloadOpacity);
+    preloaderText.classList.add(classPreloadTextAnimate);
     return new Promise((resolve) => {
       setTimeout(() => {
         preloader.classList.add(classPreloadHide);
