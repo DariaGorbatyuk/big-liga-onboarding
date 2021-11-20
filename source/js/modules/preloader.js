2a4e8c35d0e9ab623f896a17ab2ceefb111ff6c3
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-11-20 19:45:41 +0300

    Добавляет анимацию на главный экран

--- source/js/modules/preloader.js
+++ source/js/modules/preloader.js
@@ -2,7 +2,11 @@ import {resizeInit} from '../utils/resize';
 
 export function initPreloader() {
   const preloader = document.querySelector('.preloader');
+  const col2 = document.querySelector('.main-page__col-2');
+  const col1 = document.querySelector('.main-page__col-1');
   const classPreloadHide = 'preloader--hide';
+  const col1AnimationClass = 'main-page__col-1--animation';
+  const col2AnimationClass = 'main-page__col-2--animation';
 
 
   function onDisablePreloader(evt) {
@@ -12,6 +16,8 @@ export function initPreloader() {
     preloader.classList.add(classPreloadHide);
     document.removeEventListener('click', onDisablePreloader);
     document.removeEventListener('keyup', onDisablePreloader);
+    col1.classList.remove(col1AnimationClass);
+    col2.classList.remove(col2AnimationClass);
   }
 
   function setDesktopListeners() {
