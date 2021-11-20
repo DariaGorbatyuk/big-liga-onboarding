8ed3beccdd5842e24d8c26136a1908245ea6ad81
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-11-20 17:56:59 +0300

    Добавляет js для loader

---
+++ source/js/modules/preloader.js
@@ -0,0 +1,46 @@
+export function initPreloader() {
+
+  window.addEventListener('resize', onDocumentResize);
+  const preloader = document.querySelector('.preloader');
+
+  const classPreloadHide = 'preloader--hide';
+  const desktopMinWidth = 1024;
+  let isDesktopState = document.documentElement.offsetWidth >= desktopMinWidth;
+
+  function onDisablePreloader(evt) {
+    if (evt.key !== 'Enter' && isDesktopState) {
+      return;
+    }
+    preloader.classList.add(classPreloadHide);
+    document.removeEventListener('click', onDisablePreloader);
+    document.removeEventListener('keyup', onDisablePreloader);
+  }
+
+
+  function onDocumentResize() {
+    if (document.documentElement.offsetWidth >= desktopMinWidth && !isDesktopState) {
+      setDesktopListeners();
+    } else if (document.documentElement.offsetWidth < desktopMinWidth && isDesktopState) {
+      setTabletListeners();
+    }
+  }
+
+  function setDesktopListeners() {
+    document.addEventListener('keyup', onDisablePreloader);
+    document.removeEventListener('click', onDisablePreloader);
+    isDesktopState = true;
+  }
+
+  function setTabletListeners() {
+    document.addEventListener('click', onDisablePreloader);
+    document.removeEventListener('keyup', onDisablePreloader);
+    isDesktopState = false;
+  }
+
+  if (isDesktopState) {
+    setDesktopListeners();
+  } else {
+    setTabletListeners();
+  }
+
+}
